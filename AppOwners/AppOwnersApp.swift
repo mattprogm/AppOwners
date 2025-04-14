381d557c4d7d1d27ef8bc603505d813b88baa57d
mattprogm <mattprogm@gmail.com>
2025-04-14 13:16:19 -0300

    Initial Commit

---
+++ AppOwners/AppOwnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOwnersApp.swift
+//  AppOwners
+//
+//  Created by Matheus Henrique on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AppOwnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
