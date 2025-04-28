46714f0730a9124ab5d1452c88e59eeb6a8be0f9
mattprogm <mattprogm@gmail.com>
2025-04-28 12:43:26 -0300

    feat/test-dev
    testing dev branch

--- AppOwners/pinMap.c
+++ AppOwners/pinMap.c
@@ -10,6 +10,9 @@
 
 #define PIN_LED             GPIO_NUM_1
 #define PIN_LED             GPIO_NUM_2
+#define PIN_LED             GPIO_NUM_3
+#define PIN_LED             GPIO_NUM_4
+
 
 
 #define PIN_PULSE_DRY_1     GPIO_NUM_13
