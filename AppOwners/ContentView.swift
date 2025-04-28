58a8ad786872d14ffde779f87dd622babfc35f35
Rodrigo <gitdev@norf.com.br>
2025-04-28 13:08:25 -0300

    test-branch
    branch test

--- AppOwners/ContentView.swift
+++ AppOwners/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Text("NORF")      
             Text("NORF")
+
         }
         .padding()
     }
