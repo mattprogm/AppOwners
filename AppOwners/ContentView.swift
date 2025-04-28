4d86eb2c92f5f1248c5d4e50ba5a7595c8012253
Rodrigo <gitdev@norf.com.br>
2025-04-28 13:01:19 -0300

    fix: tirando-ponto-exclamação
    tirando o ponto de exclamação

--- AppOwners/ContentView.swift
+++ AppOwners/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("NORF!")
+            Text("NORF")
         }
         .padding()
     }
